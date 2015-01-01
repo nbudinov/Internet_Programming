2736dc6420f0aca859897fb58ad398d2f3616d96
nbudinov <nikkik@mail.bg>
2015-01-01 03:15:18 +0200

    end

--- 06_JQuery/WebContent/Scripts/scr.js
+++ 06_JQuery/WebContent/Scripts/scr.js
@@ -15,31 +15,34 @@ $(document).ready(function()
 	
 //task 4
 	
-	$("#menu-top-level-menu ").append('<li id="newbutID"> <a> New Button </a> </li> ')
+	//$("#menu-top-level-menu ").append('<li> <a> NewButton </a> </li> ')
+	//9nth commit
+	$("#menu-top-level-menu ").append('<li id="newbutID"> <a> New	Button </a> </li> ')
 
+	
 // task 5
 	
 	$("div#footer").prepend('<div id="dynamiccontent"></div>');
-
+	
 //task 6
-		
+	
 	$("#dynamiccontent").append('<input id= "textinput"> </input>');
-
+	
 //task 7
 	
 	$("#dynamiccontent").append('<button id="addbutton">Submit</button>');
-
+	
 //task 8
 	
 	$("#dynamiccontent").append('<ul id= "posts"></ul>');
-
+	
 //task 9
 	
-	$("#newbutID a:first").click(function()
-	{
-		alert("Hello World");		
-	});
-
+//	$("#newbutID a:first").click(function()
+//	{
+//		alert("Hello World");		
+//	});
+	
 //task 10
 	
 	$("#newbutID a:first").click(function()
@@ -48,10 +51,9 @@ $(document).ready(function()
 		$("#col2").after($(col));
 		
 	});
-	
 
 //task 11
-		
+	
 	for(var i = 1; i <= 5; i++)	{
 		$.ajax(json+"/posts/" + i,
 		{
@@ -60,142 +62,137 @@ $(document).ready(function()
 		{
 			$('#posts').append('<li>' + data.title /*data.body ..*/ + '</li>');
 		});
-	}	
-	
+	}
 	
 //task 12
-
+/*
 	$('#addbutton').click(function()
 	{
 		if($('input#textinput').val() === '') 
 			alert("You must enter text");
 	});
-
-//task 13
+*/
 	
+//task 13
+/*	
 	$('#addbutton').click(function()
-	{
-		if($('input#textinput').val() === '') 
-			alert("You must enter text");
-		else
-		{
-		//		$.post(json+"/posts", $('#textinput').val());
+			{
+				if($('input#textinput').val() === '') 
+					alert("You must enter text");
+				else
+				{
+			//		$.post(json+"/posts", $('#textinput').val());
 					
-			$.ajax({
-				type: "POST",
-				url: json+"/posts",
-				data: $('#textinput').val(),
-				success: alert("It sent successfuly")
+					$.ajax({
+						type: "POST",
+						url: json+"/posts",
+						data: $('#textinput').val(),
+						success: alert("It sent successfuly")
+					});
+				}
 			});
-		}
-	});
-
+*/
 	
 //task 14
 /*
 	$('#addbutton').click(function()
-	{
-		if($('input#textinput').val() === '') 
-			alert("You must enter text");
-		else
-		{
-			$.post(json+"/posts", { 
-				userId: 101,
-				body: $('#textinput').val()						
-			},
-			function(par)
 			{
-				$.get(json+"/posts"+par.id, function(data)
+				if($('input#textinput').val() === '') 
+					alert("You must enter text");
+				else
 				{
-					$("#posts").append("<li>" + par.body + "</li>");
-				});		
-			})		
-		}
-	});
+					$.post(json+"/posts", { 
+						userId: 10,
+						body: $('#textinput').val()						
+					},
+					function(par)
+					{
+						$.get(json+"/posts"+par.id, function(data)
+						{
+							$("#posts").append("<li>" + par.body + "</li>");
+						});		
+					})		
+				}
+			});
 */
-	
+
 //task 15
 /*
 	$('#addbutton').click(function()
-	{
-		if($('input#textinput').val() === '') 
-			alert("You must enter text");
-		else
-		{
-			$.post(json+"/posts", { 
-				userId: 101,
-				body: $('#textinput').val()						
-			},
-			function(par)
-			{
-				$.get(json+"/posts"+par.id, function(data)
 				{
-					var $ulLi = $("<li>" + par.body + "</li>")
-							
-					var $butt = $('<button> X </button>').click(function(){
-						alert('deleting');
-					});
-							
-					$ulLi.append($butt);
-							
-					$("#posts").append($ulLi);								
-				});		
-			})		
-		}
-	});
-*/	
-			
+					if($('input#textinput').val() === '') 
+						alert("You must enter text");
+					else
+					{
+						$.post(json+"/posts", { 
+							userId: 10,
+							body: $('#textinput').val()						
+						},
+						function(par)
+						{
+							$.get(json+"/posts"+par.id, function(data)
+							{
+								var $ulLi = $("<li>" + par.body + "</li>")
+								
+								var $butt = $('<button> X </button>').click(function(){
+									alert('deleting');
+								});
+								
+								$ulLi.append($butt);
+								
+								$("#posts").append($ulLi);								
+							});		
+						})		
+					}
+				});
+*/
+	
 //task 16
 
 	$('#addbutton').click(function()
-	{
-		if($('input#textinput').val() === '') 
-			alert("You must enter text");
-		else
-		{
-			$.post(json+"/posts", { 
-				userId: 101,
-				body: $('#textinput').val()						
-			},
-			function(par)
 			{
-				$.get(json+"/posts"+par.id, function(data)
+				if($('input#textinput').val() === '') 
+					alert("You must enter text");
+				else
 				{
-					var $ulLi = $("<li>" + par.body + "</li>")
-						
-					var $butt = $('<button> X </button>').click(function(){
-						alert('deleting');
-						var ok = confirm('Del ?');
-						if (ok == true)
+					$.post(json+"/posts", { 
+						userId: 10,
+						body: $('#textinput').val()		
+					},
+					function(par)
+					{
+						console.log(par.id);
+						$.get(json+"/posts"+par.id, function(data)
 						{
-							$.ajax({
-								type: "DELETE",
-								url: json+"/posts" + par.id,
-								success: $ulLi.remove()
+							var $ulLi = $("<li>" + par.body + "</li>")
+							
+							var $butt = $('<button> X </button>').click(function(){
+								alert('deleting');
+								var ok = confirm('Del ?');
+								if (ok == true)
+								{
+									$.ajax({
+										type: "DELETE",
+										url: json+"/posts/" + par.id,
+										success: $ulLi.remove()
+									});
+								}
 							});
-						}
-					});
-					$ulLi.append($butt);
-					
-					$("#posts").append($ulLi);								
-				});		
-			})		
-		}
-	});
-
-		
-		
+							$ulLi.append($butt);
+							
+							$("#posts").append($ulLi);								
+						});		
+					})		
+				}
+			});
 
 	
 //task 17
-
+	
 	$('#posts').before('<input id="field"> </input>');
-		
-		
-		
 	
 //task 18	
-	
+/*
 	$('#field').change(function(){
 		var $fieldVal = $('#field').val();
 		if( $fieldVal !== '' )
@@ -210,13 +207,61 @@ $(document).ready(function()
 			});		
 		}
 	});
+*/
+	
+//task 19
+/*
+	$('#field').change(function(){
+		var $fieldVal = $('#field').val();
+		if( $fieldVal !== '' )
+		{
+			console.log($fieldVal);
+			$.get(json + '/posts?userId=' + $fieldVal, function(data)
+			{
+				$('#posts').empty();
+				for (var it = 0; it < data.length; it++) 
+				{
+					$('#posts').append('<li>' + data[it].title + '</li>');					
+				}
+			});		
+		}
+	});
+*/
+//task 20
+		
+	$('#field').change(function(){
+		var $fieldVal = $('#field').val();
+		if( $fieldVal !== '' )
+		{
+			console.log($fieldVal);
+			$.get(json + '/posts?userId=' + $fieldVal, function(data)
+			{
+				$('#posts').empty();
+				for (var it = 0; it < data.length; it++) 
+				{
+					var $ulLi = $('<li>' + data[it].title + '</li>');
+					
+					var $butt = $('<button> X </button>').click(function(){
+						alert('deleting');
+						var ok = confirm('Del ?');
+						if (ok == true)
+						{
+							$.ajax({
+								type: "DELETE",
+								url: json+"/posts/" + $fieldVal,
+								success: $butt.parent().remove()
+							});
+						}
+					});
+					
+					$ulLi.append($butt);
 
+					$('#posts').append($ulLi);					
+				}
+			});			
+		}
+	});
 	
-	
-
-	
-	
-
 	
 	
 });
